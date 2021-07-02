d55b933d03dc5380e5f7da7e4533cba12b30587f
aQaTL <mmsoltys@outlook.com>
2021-07-02 10:41:20 +0200

    Systemd socket activation

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,39 @@ fn clap_app() -> clap::App<'static, 'static> {
 		)
 }
 
+#[cfg(all(target_os = "linux", feature = "systemd"))]
+mod systemd {
+	use anyhow::{bail, Result};
+	use std::net::TcpListener;
+	use std::os::unix::prelude::FromRawFd;
+
+	const SD_LISTEN_FDS_START: i32 = 3;
+
+	#[link(name = "systemd")]
+	extern "C" {
+		fn sd_listen_fds(unset_environment: i32) -> i32;
+	}
+
+	pub fn systemd_socket_activation() -> Result<Option<Vec<TcpListener>>> {
+		let nfds = unsafe { sd_listen_fds(false as i32) };
+		if nfds < 0 {
+			bail!(
+				"sd_listen_fds failed: {:?}",
+				std::io::Error::from_raw_os_error(nfds)
+			);
+		}
+		if nfds == 0 {
+			return Ok(None);
+		}
+
+		let listeners: Vec<TcpListener> = (SD_LISTEN_FDS_START..(SD_LISTEN_FDS_START + nfds))
+			.map(|fd| unsafe { TcpListener::from_raw_fd(fd) })
+			.collect();
+
+		Ok(Some(listeners))
+	}
+}
+
 fn main() -> Result<()> {
 	let app = clap_app().get_matches();
 	let port = app.value_of("port").unwrap();
