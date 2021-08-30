22675b1fee23c9da6f96cd06257b7c4223c5464f
aQaTL <mmsoltys@outlook.com>
2021-08-30 20:45:14 +0200

    Setup nuxt frontend

---
+++ frontend2/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div>Hello, World</div>
+        <div><input v-on:click="plusOne()" type="button" :value="this.btnLabel"></div>
+        <div>{{ counter }}</div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "index",
+
+    head: function() {
+        return {
+            title: "Share_it"
+        };
+    },
+
+    data: function() {
+        return {
+            btnLabel: "+1",
+            counter: 1,
+        };
+    },
+
+    methods: {
+        plusOne: function() {
+           this.counter++;
+        },
+    },
+});
+</script>
